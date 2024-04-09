3322bc712309534a4be0fa3d2afb79aac7707190
heyimjosh <josh@rocanews.com>
2024-04-08 21:29:34 -0400

    Initial Commit

---
+++ DontSnooze/DontSnoozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontSnoozeApp.swift
+//  DontSnooze
+//
+//  Created by Josh Davis on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DontSnoozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
